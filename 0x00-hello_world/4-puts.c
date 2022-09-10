ff72ecdb82f031183ff3caf4b228658a74fae479
root <amanephrem223@gmail.com>
2022-09-10 11:22:50 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main-Printing
+ * main - Entry point
  *
- * Return:Always 0 (Successfull)
+ * Return: 0 after printing the function
  */
 int main(void)
-{
-	puts("\"Programming is like building a multilingual 
-puzzel");
+{puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
