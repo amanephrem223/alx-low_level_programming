cbf7180bd22bc1d9bb2ec38bd244102bc9195bb6
root <amanephrem223@gmail.com>
2022-09-10 11:02:51 -0700

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-Printing
+ *
+ * Return:Always 0 (Successfull)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzel");
+	return (0);
+}
